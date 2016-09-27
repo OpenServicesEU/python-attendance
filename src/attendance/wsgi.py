6fbf3edb489059f93cee6684bf5046386f538391
Michael Fladischer <michael.fladischer@medunigraz.at>
2016-09-27 09:50:56 +0200

    Append source path in WSGI mode.

--- src/attendance/wsgi.py
+++ src/attendance/wsgi.py
@@ -5,6 +5,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 """
 
 import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(__file__, '../..')))
 
 from django.core.wsgi import get_wsgi_application
 
