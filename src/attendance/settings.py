fd84d256a901ad7085b906e203e9ae617ff4c73f
Michael Fladischer <michael.fladischer@medunigraz.at>
2016-10-03 12:43:11 +0200

    Monkey-patch debug_toolbar to show GeoIP information.

--- src/attendance/settings.py
+++ src/attendance/settings.py
@@ -241,3 +241,10 @@ LOGGING = {
         },
     },
 }
+
+from debug_toolbar.panels.headers import HeadersPanel
+
+HeadersPanel.ENVIRON_FILTER.add('GEOIP_ADDR')
+HeadersPanel.ENVIRON_FILTER.add('GEOIP_CONTINENT_CODE')
+HeadersPanel.ENVIRON_FILTER.add('GEOIP_COUNTRY_CODE')
+HeadersPanel.ENVIRON_FILTER.add('GEOIP_COUNTRY_NAME')
